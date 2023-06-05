3c8002cb62d80b2fb51de7e318da4f952d64cc2b
Mark Khramov <hramov.mark321@gmail.com>
2023-06-05 19:01:30 +0300

    refactor

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,15 +1,22 @@
 import React, {useState} from "react";
 import {SuperButton} from "../SuperButton/SuperButton";
+import s from '../SuperButton/SuperButton.module.css'
+import c from './Counter.module.css'
 
 const Counter = () => {
     const [likes, setLikes] = useState<number>(0)
+    const finalClassname = `
+    ${likes >= 5 ? c.red : ''}
+    `
     return (
         <div>
-            <div>
+            <div className={finalClassname}>
                 {likes}
             </div>
-            <div>
+            <div className={s.btn}>
                 <SuperButton callback={() => {setLikes(likes + 1)}} title={'Increment'} isDisabled={likes >= 5}/>
+            </div>
+            <div className={s.btn}>
                 <SuperButton callback={() => {setLikes(0)}} title={'Reset'} isDisabled={likes <= 0}/>
             </div>
 
